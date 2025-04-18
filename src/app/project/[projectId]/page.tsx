d00e7666cd68d6c9a4d20d0bc7a4d24b8fd0971b
0Armaan025 <armaan33000@gmail.com>
2025-04-18 19:34:43 +0530

    added backlog, tasks kanban and calendar

--- src/app/project/[projectId]/page.tsx
+++ src/app/project/[projectId]/page.tsx
@@ -8,6 +8,8 @@ import { ClipboardList, ListCheck, ListCheckIcon, ListChecksIcon, CalendarDays }
 
 import { ThemeChangeBtn } from '@/components/theme-change-btn';
 import Sidebar from '@/components/side-bar';
+import { Avatar, AvatarImage } from '@/components/ui/avatar';
+import { AvatarFallback } from '@radix-ui/react-avatar';
 
 
 
@@ -40,10 +42,23 @@ const ProjectPage = () => {
                 <div className="flex flex-row w-full min-h-screen">
 
                     <Sidebar setChosenItem={handleItemClick} chosenItem={chosenItem} />
-                    <div className="content">
+                    <div className="content flex flex-col ">
                         {chosenItem === "Backlog" && (
                             <>
-                                <h3> chosen item is backlog </h3>
+                                <h3 className='text-3xl font-semibold mt-6 ml-8'>Backlog for Project: {projectId}</h3>
+
+                                <div className="members flex flex-row">
+                                    <input type="search" placeholder='Search' className='px-6 py-3 border-1 border-gray-600 rounded-md ml-8 mt-4 w-80' />
+                                    <Avatar className='w-20 h-18'>
+                                        <AvatarImage src="https://plus.unsplash.com/premium_photo-1744193093539-8278a47ae01c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxfHx8ZW58MHx8fHx8" alt="Avatar" className='ml-4 mt-4' />
+                                        <AvatarFallback>Hi there</AvatarFallback>
+                                    </Avatar>
+                                    <Avatar className='w-20 h-18 ml-[-2rem] border-2 border-white'>
+                                        <AvatarImage src="https://plus.unsplash.com/premium_photo-1744193093539-8278a47ae01c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxfHx8ZW58MHx8fHx8" alt="Avatar" className='ml-4 mt-4' />
+                                        <AvatarFallback>Hi there</AvatarFallback>
+                                    </Avatar>
+                                </div>
+
                             </>
                         )}
                     </div>
